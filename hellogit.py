ebfcd6a7f0f5efeea90d84c7a00cae1e7f0e0235
Jasson <sp23001@ues.edu.sv>
2025-04-17 11:37:15 -0600

    etiquetas importantes

--- hellogit.py
+++ hellogit.py
@@ -1,2 +1,24 @@
+#git reset --hard [codigo]
+#git reflog
+#git checkout main
+#git tag
+#git chackout tags/[nombre]
+
+#crear nuevas ramas y moverme entre ellas
+#git branch [name rama]
+#git switch [name rama]
+#git merge main
+
+#crear un archivo temporal
+#git stash
+#git stash list
+#git stash pop
+#git stash drop
+
+#borrar una rama
+#git branch -d [nombre rama]
+
+
+
 print("Hello git")
 print("El fichero ha sido modificado")
